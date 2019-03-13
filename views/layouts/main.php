a9b0d815c47f0a29bfbcc346a9ed43f1fb07048c
mariamika <joolveg@gmail.com>
2019-03-14 01:19:44 +0300

    Изменила пункты меню. Теперь если на сайте авторизованный пользователь - отображается пункт "Выйти(имя пользователя)". Иначе отображаются пункты меню "Зарегистрироваться | Войти".

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -29,7 +29,7 @@ AppAsset::register($this);
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => Yii::$app->name,
+        'brandLabel' => 'My Tracker',
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
@@ -38,16 +38,23 @@ AppAsset::register($this);
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
+            ['label' => 'Задача', 'url' => []],
+            ['label' => 'Цели', 'url' => []],
+            ['label' => 'Задачи на день', 'url' => []],
+            ['label' => 'Списки', 'url' => []],
+            ['label' => 'Отчеты', 'url' => []],
             Yii::$app->user->isGuest ? (
-                ['label' => 'Login', 'url' => ['/site/login']]
+                '<li>'
+                . Html::a('Зарегистрироваться', [])
+                . '</li>'
+                . '<li>'
+                . Html::a('Войти', ['/site/login'])
+                . '</li>'
             ) : (
                 '<li>'
                 . Html::beginForm(['/site/logout'], 'post')
                 . Html::submitButton(
-                    'Logout (' . Yii::$app->user->identity->username . ')',
+                    'Выйти (' . Yii::$app->user->identity->username . ')',
                     ['class' => 'btn btn-link logout']
                 )
                 . Html::endForm()
@@ -58,6 +65,12 @@ AppAsset::register($this);
     NavBar::end();
     ?>
 
+    <header>
+<!--        Сюда написать что-то для шапки проекта -->
+    </header>
+
+
+<!--    Эту часть пока не трогаем, в конце добавим сюда правила использования приложения-->
     <div class="container">
         <?= Breadcrumbs::widget([
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
@@ -65,11 +78,12 @@ AppAsset::register($this);
         <?= Alert::widget() ?>
         <?= $content ?>
     </div>
+
 </div>
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+        <p class="pull-left">&copy; My Tracker 2019</p>
 
         <p class="pull-right"><?= Yii::powered() ?></p>
     </div>
